36298541bbc5b1927f4adfaa02c1fc88137d9a4c
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-06-04 11:10:21 -0300

    testing animation

---
+++ VR_Project/Assets/Scripts/ChangeMesh.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMesh : MonoBehaviour
+{
+    private MeshFilter mesh;
+    public Mesh awake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>();
+        mesh.mesh = awake;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
