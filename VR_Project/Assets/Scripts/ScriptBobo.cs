03573e1d24e0dde5315153afac4aa20daba129c3
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-06-24 00:09:00 -0300

    a mimir

---
+++ VR_Project/Assets/Scripts/ScriptBobo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptBobo : MonoBehaviour
+{
+
+    public Transform bote;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = bote.transform.position;
+    }
+}
---
+++ VR_Project/Assets/Scripts/UpdateList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateList : MonoBehaviour
+{
+
+    public TMPro.TextMeshProUGUI lista;
+    public TMPro.TextMeshProUGUI porcentagem;
+    public Pokelist poke;
+    private string a;
+    private float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a = "Capturados:\n";
+        foreach (string p in poke.poke)
+        {
+            a += p + "\n";
+        }
+        lista.text = a;
+
+        x = ( (float) poke.poke.Count /(float) poke.maxPoke ) * 100.0f;
+
+        Debug.Log(x);
+
+        porcentagem.text = x.ToString() + "%";
+    }
+}
